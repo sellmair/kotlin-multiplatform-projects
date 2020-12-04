9b2e744092676b2647d85f88468adbab6b91cd70
sebastian.sellmair <sebastian.sellmair@jetbrains.com>
2020-12-04 11:52:16 +0100

    Use other linux linker and compilerOptions?

---
+++ p2/src/macosTest/kotlin/MacosTest.kt
@@ -0,0 +1,21 @@
+import kotlinx.cinterop.toKString
+import libcurl.*
+import kotlin.test.Test
+
+@OptIn(ExperimentalUnsignedTypes::class)
+class MacosTest {
+    @Test
+    fun curl() {
+        val curl = curl_easy_init()
+        if (curl != null) {
+            curl_easy_setopt(curl, CURLOPT_URL, "http://example.com")
+            curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L)
+            val res = curl_easy_perform(curl)
+            if (res != CURLE_OK) {
+                println("curl_easy_perform() failed ${curl_easy_strerror(res)?.toKString()}")
+            }
+            curl_easy_cleanup(curl)
+        }
+    }
+}
+

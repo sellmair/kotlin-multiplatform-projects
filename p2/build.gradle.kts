c2c2e810485268d40ea663feebc3fe6a146353d7
sebastian.sellmair <sebastian.sellmair@jetbrains.com>
2020-12-14 13:39:01 +0100

    Prepare for help!

--- p2/build.gradle.kts
+++ p2/build.gradle.kts
@@ -1,19 +1,19 @@
-import org.jetbrains.kotlin.gradle.targets.native.internal.DependencyCommonizerTask
-import org.jetbrains.kotlin.gradle.targets.native.internal.commonOutput
-import org.jetbrains.kotlin.gradle.targets.native.internal.output
-
 plugins {
     kotlin("multiplatform")
 }
+
+
 
 kotlin {
     val macos = macosX64("macos")
     val linux = linuxX64("linux")
 
+    /*
     val commonizeCurl = tasks.create<DependencyCommonizerTask>("commonizeCurl") {
         addLibrary(macos, files("libs/macos_x64/curl.klib"))
         addLibrary(linux, files("libs/linux_x64/curl.klib"))
     }
+     */
 
     val commonMain = sourceSets.getByName("commonMain")
     val nativeMain = sourceSets.create("nativeMain")
@@ -29,23 +29,59 @@ kotlin {
     }
 
     nativeMain.dependencies {
-        implementation(commonizeCurl.commonOutput())
     }
 
     macosMain.dependencies {
-        implementation(commonizeCurl.commonOutput())
-        implementation(commonizeCurl.output(macos))
-        //implementation(files("libs/macos_x64/curl.klib"))
     }
 
     linuxMain.dependencies {
-        implementation(commonizeCurl.commonOutput())
-        implementation(commonizeCurl.output(linux))
-        //implementation(files("libs/linux_x64/curl.klib"))
     }
 
     sourceSets.all {
         languageSettings.useExperimentalAnnotation("kotlin.RequiresOptIn")
     }
+}
+
+//enableCommonization(setOf(MACOS_X64, LINUX_X64, IOS_ARM32))
+
+abstract class MyTransformation : TransformAction<TransformParameters.None> {
+    @get:InputArtifact
+    abstract val inputArtifact: Provider<FileSystemLocation>
+    override fun transform(output: TransformOutputs) {
+        output.file("myfile").writeText("Hey there Delilah; if you work please break the Compilah")
+    }
+}
+
+val toCommonize = Attribute.of("toCommonize", Boolean::class.javaObjectType)
+
+val curlKotlinInteropBundle by configurations.creating {
+    outgoing {
+        attributes {
+            attribute(toCommonize, true)
+        }
+    }
+
+    isCanBeResolved = false
+    isCanBeConsumed = true
+}
+
+configurations.all {
+    afterEvaluate {
+        if (this != curlKotlinInteropBundle) {
+            attributes.attribute(toCommonize, false)
+        }
+    }
+}
+
+dependencies {
+    registerTransform(MyTransformation::class.java) {
+        from.attribute(toCommonize, true)
+        to.attribute(toCommonize, false)
+    }
+}
+
+dependencies {
+    curlKotlinInteropBundle(files("libs/macos_x64/curl.klib", "libs/linux_x64/curl.klib"))
+    "nativeMainImplementation"(project(project.path, curlKotlinInteropBundle.name))
 }
 

01fc6114f1fa0f276e3538c656d585ff1256368e
sebastian.sellmair <sebastian.sellmair@jetbrains.com>
2020-12-15 10:39:26 +0100

    WIP

--- p1/build.gradle.kts
+++ p1/build.gradle.kts
@@ -1,16 +1,3 @@
 plugins {
-    kotlin("multiplatform")
-}
-
-
-kotlin {
-    val macos = macosX64("macos")
-    val linux = linuxX64("linux")
-
-    // c interop
-    listOf(macos, linux).forEach { target ->
-        val curl = target.compilations.getByName("main").cinterops.create("curl")
-        curl.defFile = file("interop/libcurl.def")
-    }
-    //
+    kotlin("multiplatform") apply false
 }
--- p2/build.gradle.kts
+++ p2/build.gradle.kts
@@ -1,3 +1,14 @@
+import org.gradle.api.attributes.Usage.USAGE_ATTRIBUTE
+import org.jetbrains.kotlin.compilerRunner.konanHome
+import org.jetbrains.kotlin.gradle.cinterop.CommonizerSelectionTransformation
+import org.jetbrains.kotlin.gradle.cinterop.CommonizerTransformation
+import org.jetbrains.kotlin.gradle.cinterop.CommonizerUsages
+import org.jetbrains.kotlin.gradle.cinterop.usage
+import org.jetbrains.kotlin.gradle.plugin.KotlinPlatformType
+import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget
+import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinUsages
+import org.jetbrains.kotlin.konan.target.KonanTarget
+
 plugins {
     kotlin("multiplatform")
 }
@@ -32,6 +43,7 @@ kotlin {
     }
 
     macosMain.dependencies {
+        //implementation(files("libs/macos_x64/curl.klib"))
     }
 
     linuxMain.dependencies {
@@ -44,44 +56,66 @@ kotlin {
 
 //enableCommonization(setOf(MACOS_X64, LINUX_X64, IOS_ARM32))
 
-abstract class MyTransformation : TransformAction<TransformParameters.None> {
-    @get:InputArtifact
-    abstract val inputArtifact: Provider<FileSystemLocation>
-    override fun transform(output: TransformOutputs) {
-        output.file("myfile").writeText("Hey there Delilah; if you work please break the Compilah")
-    }
-}
-
-val toCommonize = Attribute.of("toCommonize", Boolean::class.javaObjectType)
 
 val curlKotlinInteropBundle by configurations.creating {
     outgoing {
         attributes {
-            attribute(toCommonize, true)
+            attribute(USAGE_ATTRIBUTE, project.usage(CommonizerUsages.interopBundle))
+            attribute(KotlinNativeTarget.konanTargetAttribute, "commonizer")
+            attribute(KotlinPlatformType.attribute, KotlinPlatformType.common)
         }
     }
-
     isCanBeResolved = false
     isCanBeConsumed = true
 }
 
-configurations.all {
-    afterEvaluate {
-        if (this != curlKotlinInteropBundle) {
-            attributes.attribute(toCommonize, false)
+artifacts {
+    add("curlKotlinInteropBundle", file("libs"))
+}
+
+dependencies {
+    registerTransform(CommonizerTransformation::class) {
+        from.attribute(USAGE_ATTRIBUTE, project.usage(CommonizerUsages.interopBundle))
+        from.attribute(KotlinNativeTarget.konanTargetAttribute, "commonizer")
+
+        to.attribute(USAGE_ATTRIBUTE, project.usage(CommonizerUsages.commonizerOutput))
+        to.attribute(KotlinNativeTarget.konanTargetAttribute, "commonizer")
+
+        parameters {
+            konanHome = File(project.konanHome).absoluteFile
+            afterEvaluate {
+                commonizerClasspath = configurations.getByName("kotlinKlibCommonizerClasspath").resolve()
+            }
+            kotlin.targets.withType<KotlinNativeTarget>().all {
+                targets = targets + konanTarget
+            }
+        }
+    }
+
+    for ((_, target) in KonanTarget.predefinedTargets) {
+        registerTransform(CommonizerSelectionTransformation::class.java) {
+            from.attribute(USAGE_ATTRIBUTE, project.usage(CommonizerUsages.commonizerOutput))
+            from.attribute(KotlinNativeTarget.konanTargetAttribute, "commonizer")
+            from.attribute(KotlinPlatformType.attribute, KotlinPlatformType.common)
+
+            to.attribute(USAGE_ATTRIBUTE, project.usage(KotlinUsages.KOTLIN_API))
+            to.attribute(KotlinPlatformType.attribute, KotlinPlatformType.native)
+            to.attribute(KotlinNativeTarget.konanTargetAttribute, target.name)
+            parameters {
+                targets = setOf(target)
+            }
+        }
+    }
+
+    registerTransform(CommonizerSelectionTransformation::class.java) {
+        from.attribute(USAGE_ATTRIBUTE, project.usage(CommonizerUsages.commonizerOutput))
+        to.attribute(USAGE_ATTRIBUTE, project.usage(KotlinUsages.KOTLIN_METADATA))
+        parameters {
+            targets = setOf(KonanTarget.MACOS_X64, KonanTarget.LINUX_X64)
         }
     }
 }
 
 dependencies {
-    registerTransform(MyTransformation::class.java) {
-        from.attribute(toCommonize, true)
-        to.attribute(toCommonize, false)
-    }
-}
-
-dependencies {
-    curlKotlinInteropBundle(files("libs/macos_x64/curl.klib", "libs/linux_x64/curl.klib"))
     "nativeMainImplementation"(project(project.path, curlKotlinInteropBundle.name))
 }
-

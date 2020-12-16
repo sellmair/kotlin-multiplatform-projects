eab1e5966da430ca4bd915df9e8f0e387790cb42
sebastian.sellmair <sebastian.sellmair@jetbrains.com>
2020-12-16 14:26:53 +0100

    continue after demo

--- p1/build.gradle.kts
+++ p1/build.gradle.kts
@@ -1,5 +1,5 @@
-import org.jetbrains.kotlin.gradle.cinterop.CommonizerUsages
-import org.jetbrains.kotlin.gradle.cinterop.usage
+import org.jetbrains.kotlin.gradle.ib.CommonizerUsages
+import org.jetbrains.kotlin.gradle.ib.usage
 import org.jetbrains.kotlin.gradle.plugin.KotlinPlatformType
 import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget
 import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinUsages

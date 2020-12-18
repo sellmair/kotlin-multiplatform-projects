9f5f64ebb7b854d244789e3cfe342da3b9209d5f
sebastian.sellmair <sebastian.sellmair@jetbrains.com>
2020-12-18 17:01:30 +0100

    Implement interop-bundle publishing

--- p2/build.gradle.kts
+++ p2/build.gradle.kts
@@ -6,8 +6,8 @@ plugins {
 //region Project Structure
 
 kotlin {
-    val macos = macosX64("macos")
-    val linux = linuxX64("linux")
+    macosX64("macos")
+    linuxX64("linux")
 
     val commonMain = sourceSets.getByName("commonMain")
     val nativeMain = sourceSets.create("nativeMain")
@@ -19,33 +19,14 @@ kotlin {
     linuxMain.dependsOn(nativeMain)
 
     commonMain.dependencies {
-        implementation(kotlin("stdlib-common"))
-    }
+        // option 1
+        implementation("io.sellmair:libcurl:7.64.1-kib0")
 
-    nativeMain.dependencies {
-        implementation(project(":p1"))
-    }
-
-    macosMain.dependencies {
-    }
-
-    linuxMain.dependencies {
+        // option 2 (also works)
+        //implementation(project(":p1"))
     }
 
     sourceSets.all {
         languageSettings.useExperimentalAnnotation("kotlin.RequiresOptIn")
-    }
-}
-
-tasks.create("attrs") {
-    doLast {
-        configurations.all {
-            println("Configuration $name")
-            println("Attributes: ${this.attributes}")
-            println("Extends: ${this.extendsFrom.map { it.name }}")
-            println("isCanBeConsumed: ${this.isCanBeConsumed}")
-            println("isCanBeResolved: ${this.isCanBeResolved}")
-            println("________________________________")
-        }
     }
 }

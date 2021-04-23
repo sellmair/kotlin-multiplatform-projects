eea8933db6c6ed05d7935b46337412b020eb4acf
sebastian.sellmair <sebastian.sellmair@jetbrains.com>
2021-04-23 23:00:47 +0200

    Fix test

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,9 @@
+plugins {
+    kotlin("multiplatform") apply false
+    id("com.android.library") apply false
+    id("com.android.application") apply false
+}
+
 allprojects {
     repositories {
         mavenCentral()

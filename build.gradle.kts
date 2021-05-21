250edbfb10705c55d118a217cfe8631b48820f82
sebastian.sellmair <sebastian.sellmair@jetbrains.com>
2021-05-21 09:33:52 +0200

    Create Sample/Integration test project for KT-46856

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,20 @@
+import org.jetbrains.kotlin.gradle.plugin.mpp.AbstractKotlinNativeTargetPreset
+
+plugins {
+    kotlin("multiplatform")
+}
+
+kotlin {
+    presets.forEach { preset ->
+        if (preset is AbstractKotlinNativeTargetPreset) {
+            targetFromPreset(preset)
+        }
+    }
+}
+
 allprojects {
     repositories {
-        mavenCentral()
-        google()
         mavenLocal()
-        jcenter()
+        mavenCentral()
     }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,24 +1,11 @@
 pluginManagement {
     repositories {
+        mavenLocal()
         gradlePluginPortal()
         mavenCentral()
-        google()
-        mavenLocal()
     }
     plugins {
         kotlin("multiplatform") version "1.5.255-SNAPSHOT"
-        kotlin("android") version "1.5.255-SNAPSHOT"
-    }
-    resolutionStrategy {
-        eachPlugin {
-            if (requested.id.id.startsWith("com.android")) {
-                useModule("com.android.tools.build:gradle:4.0.2")
-            }
-        }
     }
 }
-
-rootProject.name = "mpp-issue-bootstrap"
-include(":p1")
-include(":p2")
 

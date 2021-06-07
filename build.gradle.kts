542fa7a98e8f12819b7f48863d54d2a688750f2b
Pavel Kirpichenkov <pavel.kirpichenkov@jetbrains.com>
2021-06-07 15:15:59 +0300

    Create integration test for KT-46716

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,54 @@
+import org.jetbrains.kotlin.gradle.plugin.sources.DefaultKotlinSourceSet
+
 plugins {
-    kotlin("multiplatform") apply false
+    kotlin("multiplatform")
 }
 
+repositories {
+    mavenLocal()
+    mavenCentral()
+}
 
-allprojects {
-    repositories {
-        mavenCentral()
-        google()
-        mavenLocal()
-        jcenter()
+val integrationTestTask = tasks.create("checkPlatformDependencies")
+
+fun createPlatformDependenciesTestTask(sourceSet: DefaultKotlinSourceSet) {
+    tasks.create("check${sourceSet.name.capitalize()}PlatformDependencies") {
+        integrationTestTask.dependsOn(this)
+
+        // Dependencies forwarded to the IDE will be attached to the intransitiveMetadataConfiguration
+        val dependenciesConfiguration = configurations.getByName(sourceSet.intransitiveMetadataConfigurationName)
+        dependsOn("commonize")
+
+        doLast {
+            val dependencies = dependenciesConfiguration.files
+
+            dependencies.forEach { dependency ->
+                logger.quiet("${sourceSet.name} dependency: ${dependency.path}")
+            }
+
+            if (dependencies.isEmpty()) {
+                throw AssertionError("${sourceSet.name}: Expected at least one platform dependency")
+            }
+
+            if (dependencies.none { dependency -> "/klib/platform/" in dependency.path }) {
+                throw AssertionError("${sourceSet.name}: Expected at least one dependency from '/klib/platform'")
+            }
+
+            dependencies.forEach { dependency ->
+                if ("/klib/commonized" in dependency.path) {
+                    throw AssertionError(
+                        "${sourceSet.name}: Found unexpected commonized dependency ${dependency.path}"
+                    )
+                }
+            }
+        }
     }
 }
 
-/*project(":p2").tasks.configureEach {
-    // 🤷‍ It's a demo. Who really cares?
-    dependsOn(":p1:publishToMavenLocal")
-}*/
+kotlin {
+    linuxX64()
+    linuxArm64()
+
+    createPlatformDependenciesTestTask(sourceSets.getByName("linuxX64Main") as DefaultKotlinSourceSet)
+    createPlatformDependenciesTestTask(sourceSets.getByName("linuxArm64Main") as DefaultKotlinSourceSet)
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,16 +8,6 @@ pluginManagement {
     }
     plugins {
         kotlin("multiplatform") version kotlinVersion
-        kotlin("android") version kotlinVersion
-    }
-    resolutionStrategy {
-        eachPlugin {
-            if (requested.id.id.startsWith("com.android")) {
-                useModule("com.android.tools.build:gradle:4.0.2")
-            }
-        }
     }
 }
 
-rootProject.name = "mpp-issue-bootstrap"
-include(":p2")

b1b93d39d729a97df1e4f1ae976bb184d8a793f7
sebastian.sellmair <sebastian.sellmair@jetbrains.com>
2021-08-18 14:25:43 +0200

    Add sample project

--- p1/src/jvmMain/kotlin/JvmMain.kt
+++ p1/src/jvmMain/kotlin/JvmMain.kt
@@ -3,6 +3,6 @@ actual interface CommonMainInterface {
 }
 
 actual class CommonMainImpl : CommonMainInterface {
-    actual override val propertyFromInterface: Int = 42
+    override val propertyFromInterface: Int = 42
     actual val propertyFromImpl: Int = 42
 }

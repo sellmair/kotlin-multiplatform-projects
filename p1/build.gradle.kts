4ceea27f01776b986d957ce5916a4e33d747a907
sebastian.sellmair <sebastian.sellmair@jetbrains.com>
2021-08-27 08:49:39 +0200

    Sample Sample Sample :)

--- p1/build.gradle.kts
+++ p1/build.gradle.kts
@@ -1,23 +1,14 @@
 plugins {
-    //id("com.android.library")
     kotlin("multiplatform")
 }
 
-/*
-android {
-    compileSdkVersion(30)
-}
-
- */
-
 kotlin {
-    //android()
     macosX64()
     ios()
 
     val commonTest by sourceSets.getting
     
     commonTest.dependencies {
-        implementation("io.mockk:mockk:1.11.0")
+        implementation("io.mockk:mockk:1.11.0") // <-- MOCKK is not available for ios or macos!
     }
 }

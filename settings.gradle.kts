a2ad4f7b23a865b0fcadca7980da61b5c72e76ff
sebastian.sellmair <sebastian.sellmair@jetbrains.com>
2022-06-09 16:20:56 +0200

    Update Gradle, AGP and KGP

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,13 +6,13 @@ pluginManagement {
         mavenLocal()
     }
     plugins {
-        kotlin("multiplatform") version "1.6.255-SNAPSHOT"
-        kotlin("android") version "1.6.255-SNAPSHOT"
+        kotlin("multiplatform") version "1.7.255-SNAPSHOT"
+        kotlin("android") version "1.7.255-SNAPSHOT"
     }
     resolutionStrategy {
         eachPlugin {
             if (requested.id.id.startsWith("com.android")) {
-                useModule("com.android.tools.build:gradle:4.0.2")
+                useModule("com.android.tools.build:gradle:7.1.0")
             }
         }
     }

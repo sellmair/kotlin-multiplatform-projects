7dab89aedf967cd1efe061d999de60cb91c09f7f
sebastian.sellmair <sebastian.sellmair@jetbrains.com>
2022-07-08 15:54:16 +0200

    ~

--- multiplatformAndroidApp/src/commonTest/kotlin/MultiplatformAndroidApp_commonTest.kt
+++ multiplatformAndroidApp/src/commonTest/kotlin/MultiplatformAndroidApp_commonTest.kt
@@ -4,14 +4,14 @@ object MultiplatformAndroidApp_commonTest {
     fun sayHello() {
         "some/path/".toPath()
 
-        MultiplatformAndroidJvmIosLibrary_commonMain.sayHello()
-        MultiplatformAndroidJvmIosLibrary_androidAndJvmMain.sayHello()
+        MultiplatformAndroidJvmIosLibrary_commonMain.sayHello() // unresolved; !bug in single android target case
+        MultiplatformAndroidJvmIosLibrary_androidAndJvmMain.sayHello() // unresolved; !bug in single android target case
         //MultiplatformAndroidJvmIosLibrary_androidMain.sayHello() // unresolved; would be desirable
 
         MultiplatformJvmLibrary_commonMain.sayHello()
         MultiplatformJvmLibrary_jvmMain.sayHello()
 
-        MultiplatformAndroidLibrary_commonMain.sayHello()
+        MultiplatformAndroidLibrary_commonMain.sayHello() // // unresolved; !bug in single android target case
         //MultiplatformAndroidLibrary_androidMain.sayHello() // unresolved; would be desirable
     }
 }

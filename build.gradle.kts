bee216b9046e24322140fc043e4a39ef911b3299
sebastian.sellmair <sebastian.sellmair@jetbrains.com>
2022-08-04 13:25:33 +0200

    TCS Multiplatform/Android SourceSetLayout V1 example

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,27 +36,45 @@ kotlin {
     val commonMain by sourceSets.getting
     val commonTest by sourceSets.getting
     val androidMain by sourceSets.getting
-    val androidUnitTest by sourceSets.getting
-    val androidInstrumentedTest by sourceSets.getting
+    val androidTest by sourceSets.getting
+    val androidAndroidTest by sourceSets.getting
 
     commonTest.dependencies {
         implementation(kotlin("test-junit"))
     }
 
-    androidUnitTest.dependencies {
+    androidTest.dependencies {
         implementation("org.robolectric:robolectric:4.8")
         implementation("androidx.test:core:1.4.0")
         implementation("androidx.test:core-ktx:1.4.0")
     }
 
-    androidInstrumentedTest.dependencies {
+    androidAndroidTest.dependencies {
         implementation("androidx.test:runner:1.4.0")
         implementation("androidx.test:rules:1.4.0")
     }
 
-    sourceSets.invokeWhenCreated("androidUnitTestGermanFreeDebug") {
+    sourceSets.invokeWhenCreated("androidTestGermanFreeDebug") {
         dependencies {
             implementation("com.squareup.okio:okio:3.2.0")
+        }
+    }
+}
+
+tasks.create("printAndroidSourceSetNames") {
+    doLast {
+        android.libraryVariants.all {
+            logger.quiet(name)
+            logger.quiet(sourceSets.map { it.name }.toString())
+            logger.quiet("\n")
+        }
+    }
+}
+
+tasks.create("printKotlinSourceSetNames") {
+    doLast {
+        kotlin.android().compilations.forEach {
+            logger.quiet("${it.compilationName} -> ${it.defaultSourceSetName}")
         }
     }
 }

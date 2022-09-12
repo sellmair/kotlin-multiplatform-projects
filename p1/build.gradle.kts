c14efda6cad3dd558af4a8ceeef96f42eab65149
Sebastian Sellmair <sebastian.sellmair@jetbrains.com>
2022-09-12 15:38:33 +0200

    Sample project

--- p1/build.gradle.kts
+++ p1/build.gradle.kts
@@ -1,20 +1,25 @@
 plugins {
-    id("com.android.library")
     kotlin("multiplatform")
 }
 
-android {
-    compileSdkVersion(30)
-}
-
 kotlin {
+    linuxX64()
+    macosX64()
+    ios()
     jvm()
-    android()
-    sourceSets {
-        named("androidAndroidTest") {
-            dependencies {
-                implementation(project(":p2"))
-            }
-        }
+
+    val commonMain by sourceSets.getting
+    val iosMain by sourceSets.getting
+    val linuxX64Main by sourceSets.getting
+    val macosX64Main by sourceSets.getting
+
+    val nativeMain by sourceSets.creating
+    nativeMain.dependsOn(commonMain)
+    iosMain.dependsOn(nativeMain)
+    linuxX64Main.dependsOn(nativeMain)
+    macosX64Main.dependsOn(nativeMain)
+
+    commonMain.dependencies {
+        implementation("io.ktor:ktor-client-core:+")
     }
 }
--- p2/build.gradle.kts
+++ p2/build.gradle.kts
@@ -1,8 +1,26 @@
 plugins {
-    id("com.android.library")
-    kotlin("android")
+    kotlin("multiplatform")
 }
 
-android {
-    compileSdkVersion(30)
+kotlin {
+    linuxX64()
+    macosX64()
+    ios()
+    jvm()
+
+    val commonMain by sourceSets.getting
+    val iosMain by sourceSets.getting
+    val linuxX64Main by sourceSets.getting
+    val macosX64Main by sourceSets.getting
+    val jvmMain by sourceSets.getting
+
+    val nativeMain by sourceSets.creating
+    nativeMain.dependsOn(commonMain)
+    iosMain.dependsOn(nativeMain)
+    linuxX64Main.dependsOn(nativeMain)
+    macosX64Main.dependsOn(nativeMain)
+
+    commonMain.dependencies {
+        implementation("io.ktor:ktor-client-core:+")
+    }
 }

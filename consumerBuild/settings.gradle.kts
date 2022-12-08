e6f7ac3f4a48b39945a0b67dad3a2f4dcab84b86
Sebastian Sellmair <sebastian.sellmair@jetbrains.com>
2022-12-08 14:25:01 +0100

    Setup project with composite builds

---
+++ consumerBuild/settings.gradle.kts
@@ -0,0 +1,26 @@
+pluginManagement {
+    repositories {
+        mavenLocal()
+        gradlePluginPortal()
+        mavenCentral()
+        google()
+    }
+    plugins {
+        kotlin("multiplatform") version "1.8.255-SNAPSHOT"
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        mavenLocal()
+        mavenCentral()
+    }
+}
+
+include(":consumerA")
+
+includeBuild("../producerBuild") {
+    dependencySubstitution {
+        substitute(module("org.jetbrains.sample:producerA")).using(project(":producerA"))
+    }
+}
---
+++ producerBuild/settings.gradle.kts
@@ -0,0 +1,20 @@
+pluginManagement {
+    repositories {
+        mavenLocal()
+        gradlePluginPortal()
+        mavenCentral()
+        google()
+    }
+    plugins {
+        kotlin("multiplatform") version "1.8.255-SNAPSHOT"
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        mavenLocal()
+        mavenCentral()
+    }
+}
+
+include(":producerA")

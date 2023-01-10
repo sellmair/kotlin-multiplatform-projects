fde89d30d0647d7342d79aca55f61f0a2d4db00c
Sebastian Sellmair <sebastian.sellmair@jetbrains.com>
2023-01-10 12:51:04 +0100

    Update to Gradle 7.6

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,16 +8,11 @@ pluginManagement {
     plugins {
         kotlin("multiplatform") version "1.8.255-SNAPSHOT"
         kotlin("android") version "1.8.255-SNAPSHOT"
+        id("com.android.application") version "7.3.1"
+        id("com.android.library") version "7.3.1"
     }
-    resolutionStrategy {
-        eachPlugin {
-            if (requested.id.id.startsWith("com.android")) {
-                useModule("com.android.tools.build:gradle:7.2.2")
-            }
-        }
-    }
+
 }
 
 include(":p1")
 include(":p2")
-

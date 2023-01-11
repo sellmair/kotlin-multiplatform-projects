902915992a8e245822154149402a2b5594da680e
Sebastian Sellmair <sebastian.sellmair@jetbrains.com>
2023-01-11 12:45:29 +0100

    Add reproducer

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,18 +1,29 @@
 pluginManagement {
     repositories {
+        maven("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/bootstrap/")
         gradlePluginPortal()
         mavenCentral()
         google()
         mavenLocal()
     }
     plugins {
-        kotlin("multiplatform") version "1.8.255-SNAPSHOT"
-        kotlin("android") version "1.8.255-SNAPSHOT"
+        val kv = "1.8.255-SNAPSHOT"
+        kotlin("multiplatform") version kv
+        kotlin("android") version kv
         id("com.android.application") version "7.3.1"
         id("com.android.library") version "7.3.1"
     }
 
 }
 
+dependencyResolutionManagement {
+    repositories {
+        mavenCentral()
+        google()
+        maven("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/bootstrap/")
+        mavenLocal()
+    }
+}
+
 include(":p1")
-include(":p2")
+//include(":p2")

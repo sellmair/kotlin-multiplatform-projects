665e5d6833dff09bad4794b70903af7c68315bde
Sebastian Sellmair <sebastian.sellmair@jetbrains.com>
2023-02-03 10:36:23 +0100

    Update to 1.9

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,17 +1,25 @@
 pluginManagement {
     repositories {
-        gradlePluginPortal()
-        mavenCentral()
-        google()
         mavenLocal()
+        mavenCentral()
+        gradlePluginPortal()
+        google()
     }
     plugins {
-        kotlin("multiplatform") version "1.8.255-SNAPSHOT"
-        kotlin("android") version "1.8.255-SNAPSHOT"
+        kotlin("multiplatform") version "1.9.255-SNAPSHOT"
+        kotlin("android") version "1.9.255-SNAPSHOT"
         id("com.android.application") version "7.3.1"
         id("com.android.library") version "7.3.1"
     }
 
+}
+
+dependencyResolutionManagement {
+    repositories {
+        mavenLocal()
+        mavenCentral()
+        google()
+    }
 }
 
 include(":p1")
